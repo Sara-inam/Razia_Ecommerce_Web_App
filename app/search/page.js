d4ce57d638d5179028b9cab75f454cc477257db7
Sara-inam <sarainam60@gmail.com>
2026-04-14 09:09:22 +0500

    fix search suspense issue

--- app/search/page.js
+++ app/search/page.js
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import SearchClient from "./SearchClient";
 
 export default function Page() {
-  return <SearchClient />;
+  return (
+    <Suspense fallback={<div>Loading search...</div>}>
+      <SearchClient />
+    </Suspense>
+  );
 }
